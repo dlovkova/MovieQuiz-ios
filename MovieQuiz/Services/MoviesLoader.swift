96e4e634c32ab430312a8bb82ba82acb294160ab
Daria Lovkova <yellow.danielle@gmail.com>
2023-07-21 19:34:43 +0400

    sprint 6

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -25,8 +25,25 @@ struct MoviesLoader: MoviesLoading {
         }
     
     
+//    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+//        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+//            switch result {
+//            case .success(let data):
+//                do {
+//                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+//                    handler(.success(mostPopularMovies))
+//                } catch {
+//                    handler(.failure(error))
+//                }
+//            case .failure(let error):
+//                handler(.failure(error))
+//            }
+//        }
+//    }
+    
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
-        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+
+        networkClient.fetch(url: mostPopularMoviesUrl) {result in
             switch result {
             case .success(let data):
                 do {
@@ -38,7 +55,7 @@ struct MoviesLoader: MoviesLoading {
             case .failure(let error):
                 handler(.failure(error))
             }
+
         }
     }
-    
 }
